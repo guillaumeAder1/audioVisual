3c14f1db813b3b040c2f9fa59530bcedd6bdb628
Guillaume Ader <GAder@esriuk.com>
2018-07-13 13:57:41 +0100

    Updates

--- src/component/config.js
+++
@@ -1,9 +0,0 @@
-export default {
-    files: [
-        { src: "120_MaracasLeather_SP_01.wav" },
-        { src: "70_BongoFreestyle_SP_01.wav" },
-        { src: "90_CongaOchosi_SP_03.wav" },
-        { src: "120_MamboBellHi_SP_01.wav" },
-    ],
-    root: "/audio/"
-}
